37ed9c85dc90bcc74083f3612deb3f18daef0f02
adamsyy <adamoommenjacob@gmail.com>
2022-01-01 23:26:37 +0530

    tipping

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -10,10 +10,17 @@ struct Post{
     uint id;
     string content;
     uint tipAmount;
-    address owner;
+    address payable author;
 }
 
-event PostCreated(uint id, string content, uint tipAmount, address owner);
+event PostCreated(uint id, string content, uint tipAmount, address payable author);
+
+   event PostTipped(
+        uint id,
+        string content,
+        uint tipAmount,
+        address payable author
+    );
 
         constructor()public{
         name="adam ahne";
@@ -28,9 +35,20 @@ posts[postCount]=Post(postCount,_content,0,msg.sender);
 emit PostCreated(postCount,_content,0,msg.sender);
 
    }  
-
-   function tipPost(uint _id)public{
-const post = await posts(_id);
-
-   } 
+function tipPost(uint _id) public payable {
+   require(_id > 0 && _id <= postCount);
+        // Fetch the post
+        Post memory _post = posts[_id];
+        // Fetch the author
+        address payable _author = _post.author;
+        // Pay the author by sending them Ether
+        address(_author).transfer(msg.value);
+        // Incremet the tip amount
+        _post.tipAmount = _post.tipAmount + msg.value;
+        // Update the post
+        posts[_id] = _post;
+        // Trigger an event
+        emit PostTipped(postCount, _post.content, _post.tipAmount, _author);
+    }
+   
 }          
